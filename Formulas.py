7f01ae5dde9907e7939a04c77e30952748fe8482
Audrey Teles dos Santos <68817384+Audrey-Teles@users.noreply.github.com>
2022-04-17 21:04:57 -0300

    Inserção das formulas e suas validações

---
+++ Formulas.py
@@ -0,0 +1,49 @@
+from tkinter import messagebox
+
+
+def celsiusFahrenheit(c):
+    try:
+        f = (float(c) * 1.8) + 32
+        return f
+    except:
+        messagebox.showerror("Erro", "Informe um valor válido")
+
+
+def celsiusKelvin(c):
+    try:
+        k = float(c) + 273
+        return k
+    except:
+        messagebox.showerror("Erro", "Informe um valor válido")
+
+
+def fahrenheitCelsius(f):
+    try:
+        c = (float(f) - 32)/1.8
+        return c
+    except:
+        messagebox.showerror("Erro", "Informe um valor válido")
+
+
+def fahrenheitKelvin(f):
+    try:
+        k = ((float(f) - 32) * 5/9) + 273
+        return k
+    except:
+        messagebox.showerror("Erro", "Informe um valor válido")
+
+def kelvinFahrenheit(k):
+    try:
+        f = ((float(k) - 273) * 9/5) + 32
+        return f
+    except:
+        messagebox.showerror("Erro", "Informe um valor válido")
+
+
+def kelvinCelsius(k):
+    try:
+        c = float(k) - 273
+        return c
+    except:
+        messagebox.showerror("Erro", "Informe um valor válido")
+
